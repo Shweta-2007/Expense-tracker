dba1fe478ed14a91387c0f4777164a753cda60e0
Shweta-2007 <sinhashweta713@gmail.com>
2023-12-01 16:33:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './components/Header'
+import './index.css'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
+import { GlobalProvider } from './context/GlobalState'
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header></Header>
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
+    </GlobalProvider>
+  )
+}
+
+export default App
